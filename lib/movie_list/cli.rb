81e9ac9c65c6f28dc5648759086a319a07922412
Noah A Klem <noahaklem@gmail.com>
2021-12-04 14:49:05 -0700

    Add CLI logic w validations

--- lib/movie_list/cli.rb
+++ lib/movie_list/cli.rb
@@ -1,16 +1,45 @@
 class CLI
   def call
-    puts "Hi! Welcome to Movie List!"
-    puts "I am going to help you find trending movies or shows and where to watch them."
-    puts "Please enter your name to get started."
+    puts "Hi! Welcome to Movie List! \n"
+
+    puts "\nI am going to help you find trending movies or shows and where to watch them."
+    get_user_name
+    select_movies_or_shows
+    get_user_selection
+  end
+
+  def get_user_name
+    puts "\nPlease enter your name to get started.\n"
 
     name = gets.strip
-    puts "Great, #{name}! Now, what would you like to see?"
+    puts "\nGreat, #{name}! Now, what would you like to see?\n"
   end
 
   def select_movies_or_shows
-    @selection = ["1. Trending Movies", "2. Trending Shows"]
+    @options = ["Trending Movies", "Trending Shows"]
+    @options.each.with_index(1) do |option, index|
+      puts "#{index}. #{option}"
+    end
   end
 
+  def convert_selection(selection)
+    selection.to_i 
+  end
+
+  def valid_selection(number, data)
+    number <= data.length && number > 0
+  end
+
+  def get_user_selection
+    puts "\nType: 1 for 'Trending Movies'\n or \nType: 2 for 'Trending Shows'\n"
+    selection = gets.strip
+    number = convert_selection(selection)
+    if  valid_selection(number, @options)
+      puts "worked"
+    else
+      puts "\nTry again! Remember use numbers to make a selection.\n"
+      get_user_selection
+    end
+  end
   
 end
